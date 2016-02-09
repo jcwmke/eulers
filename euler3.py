a9970f7795c211ab7decc3a2f1c4a111c14202b9
jcwmke <jcwallman@gmail.com>
2016-02-09 08:38:38 -0600

    Update euler3.py

--- euler3.py
+++ euler3.py
@@ -1,6 +1,17 @@
 ##The prime factors of 13195 are 5, 7, 13 and 29.
 ##What is the largest prime factor of the number 600851475143 ?
-from primeness import isprime
+
+def isprime(number):
+    if number > 1:
+        if number == 2:
+            return True
+        if number % 2 == 0:
+            return False
+        for current in range(3, int(math.sqrt(number) + 1), 2):
+            if number % current == 0: 
+                return False
+        return True
+    return False
 
 n = 600851475143
 
@@ -11,6 +22,4 @@ while counter < int(n**.5)+1:
         print n/counter, isprime(n/counter)
         print counter, isprime(counter)
     counter +=1
-
-isprime(7)
-isprime(11)
+        
